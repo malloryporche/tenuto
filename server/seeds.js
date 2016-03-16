2da693569c049ae2efabe69e14947d10b0b5c9e6
Mallory Porche <malloryburke@gmail.com>
2016-03-15 20:47:01 -0700

    Added projectSeeds

--- server/seeds.js
+++ server/seeds.js
@@ -2,6 +2,13 @@ var projectSeeds = [
  {
 	'name' : 'Roca Lisa',
 	'thumb' : '/images/RosaLisaThumb.jpg',
+	'altimage1': '/images/RS1.jpg',
+	'altimage2': '/images/RS2.jpg',
+	'altimage3': '/images/RS3.jpg',
+	'altimage4': '/images/RS4.jpg',
+	'altimage5': '/images/RS5.jpg',
+	'altimage6': '/images/RS6.jpg',
+	'altimage7': '/images/RS7.jpg',
 	'location' : 'Ibiza, Spain',
 	'description' : 'A modern villa located on the island of Ibiza. The architecture combines contemporary elements with natural materials. The interior flows onto outdoor terraces capturing views of the Mediterranean.',
 	'status' : 'Completed 2014',
@@ -12,6 +19,7 @@ var projectSeeds = [
  {
  	'name' : 'Roca Lisa',
 	'thumb' : '/images/RosaLisaThumb.jpg',
+	'altimage1': '/images/RS.jpg'
 	'location' : 'Ibiza, Spain',
 	'description' : 'A modern villa located on the island of Ibiza. The architecture combines contemporary elements with natural materials. The interior flows onto outdoor terraces capturing views of the Mediterranean.',
 	'status' : 'Completed 2014',
@@ -22,6 +30,7 @@ var projectSeeds = [
  {
  	'name' : 'Roca Lisa',
 	'thumb' : '/images/RosaLisaThumb.jpg',
+	'altimage1': '/images/RS.jpg'
 	'location' : 'Ibiza, Spain',
 	'description' : 'A modern villa located on the island of Ibiza. The architecture combines contemporary elements with natural materials. The interior flows onto outdoor terraces capturing views of the Mediterranean.',
 	'status' : 'Completed 2014',
@@ -32,6 +41,7 @@ var projectSeeds = [
 {
 	'name' : 'Roca Lisa',
 	'thumb' : '/images/RosaLisaThumb.jpg',
+	'altimage1': '/images/RS.jpg'
 	'location' : 'Ibiza, Spain',
 	'description' : 'A modern villa located on the island of Ibiza. The architecture combines contemporary elements with natural materials. The interior flows onto outdoor terraces capturing views of the Mediterranean.',
 	'status' : 'Completed 2014',
@@ -42,6 +52,7 @@ var projectSeeds = [
 {
 	'name' : 'Roca Lisa',
 	'thumb' : '/images/RosaLisaThumb.jpg',
+	'altimage1': '/images/RS.jpg'
 	'location' : 'Ibiza, Spain',
 	'description' : 'A modern villa located on the island of Ibiza. The architecture combines contemporary elements with natural materials. The interior flows onto outdoor terraces capturing views of the Mediterranean.',
 	'status' : 'Completed 2014',
@@ -52,6 +63,7 @@ var projectSeeds = [
 {
 	'name' : 'Roca Lisa',
 	'thumb' : '/images/RosaLisaThumb.jpg',
+	'altimage1': '/images/RS.jpg'
 	'location' : 'Ibiza, Spain',
 	'description' : 'A modern villa located on the island of Ibiza. The architecture combines contemporary elements with natural materials. The interior flows onto outdoor terraces capturing views of the Mediterranean.',
 	'status' : 'Completed 2014',
@@ -62,6 +74,7 @@ var projectSeeds = [
 {
 	'name' : 'Roca Lisa',
 	'thumb' : '/images/RosaLisaThumb.jpg',
+	'altimage1': '/images/RS.jpg'
 	'location' : 'Ibiza, Spain',
 	'description' : 'A modern villa located on the island of Ibiza. The architecture combines contemporary elements with natural materials. The interior flows onto outdoor terraces capturing views of the Mediterranean.',
 	'status' : 'Completed 2014',
@@ -72,6 +85,7 @@ var projectSeeds = [
 {
 	'name' : 'Roca Lisa',
 	'thumb' : '/images/RosaLisaThumb.jpg',
+	'altimage1': '/images/RS.jpg'
 	'location' : 'Ibiza, Spain',
 	'description' : 'A modern villa located on the island of Ibiza. The architecture combines contemporary elements with natural materials. The interior flows onto outdoor terraces capturing views of the Mediterranean.',
 	'status' : 'Completed 2014',
@@ -80,3 +94,10 @@ var projectSeeds = [
 	'type' : 'Single Residential',
 }
 ]
+
+if(Projects.find().count() === 0 ) {
+	_.each(projectSeeds, function(project){
+		Projects.insert(project);
+		console.log('Inserted ', project.name)
+	})
+}
